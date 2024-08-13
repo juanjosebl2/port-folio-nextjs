4cfb9bb9a1c414b0628953c023817cc59f13ba10
juanjosebl2 <juanjosebl2@gmail.com>
2024-08-13 13:08:56 +0200

    finish paragraph works

--- app/work/components/Work.data.ts
+++ app/work/components/Work.data.ts
@@ -1,35 +1,93 @@
 export const dataWorks = [
     {
         num: "01",
-        category: "Desarrollo Web",
-        title: "Proyecto de pelugueria para un cliente",
-        description: "Una descricopn de mi",
-        stack: [{ name: "React" }, { name: "Tailwind" }, { name: "Nextjs" }],
+        category: "Aplicación Web Fullstack",
+        title: "Plataforma Web para Gestión de Servicios en Peluquería",
+        description: "Desarrollo de una página web personalizada para una peluquería que necesitaba una plataforma digital para mostrar sus servicios, gestionar citas en línea y permitir la incorporación de otros estilistas.",
+        stack: [{ name: "Typescript" }, { name: "Tailwind" }, { name: "Nextjs" }, { name: "Laravel" }, { name: "Mysql" }],
         name: "Javascript",
-        image: "/assets/work/thumb1.png",
-        live: "",
+        image: "/assets/work/work1.png",
+        live: "https://josue-hair-studio.vercel.app/",
         github: ""
     },
     {
         num: "02",
-        category: "Desarrollo Web",
-        title: "Proyecto de pelugueria para un cliente",
-        description: "Una descricopn de mi",
-        stack: [{ name: "React" }, { name: "Tailwind" }, { name: "Nextjs" }],
-        name: "Javascript",
-        image: "/assets/work/thumb1.png",
-        live: "",
-        github: ""
+        category: "Aplicación Web Fullstack",
+        title: "Proyecto para la gestión de alquiler de coches",
+        description: "Desarrollo de una pagina web personalizada para una empresa de alquiler de coches, donde el cliente puede realizar reservas, ver los coches disponibles y gestionar su cuenta, incluye metodo de pago.",
+        stack: [{ name: "Typescript" }, { name: "Tailwind" }, { name: "Nextjs" }, { name: "Prisma" }, { name: "NodeJs" }],
+        image: "/assets/work/work2.png",
+        live: "https://drive-x-by-juan.vercel.app/",
+        github: "https://github.com/juanjosebl2/DriveX"
     },
     {
         num: "03",
-        category: "Desarrollo Web",
-        title: "Proyecto de pelugueria para un cliente",
-        description: "Una descricopn de mi",
-        stack: [{ name: "React" }, { name: "Tailwind" }, { name: "Nextjs" }],
-        name: "Javascript",
-        image: "/assets/work/thumb1.png",
+        category: "Aplicación Web Frontend",
+        title: "Pagina web moderna para un banco",
+        description: "Desarrollo de una pagina web formada por un curso, donde se muestra la información de un banco, con un diseño moderno y atractivo.",
+        stack: [{ name: "Typescript" }, { name: "Tailwind" }, { name: "Nextjs" }],
+        image: "/assets/work/work3.png",
+        live: "https://bank-nextjs-by-juan.vercel.app/",
+        github: "https://github.com/juanjosebl2/bank-nextjs"
+    },
+    {
+        num: "04",
+        category: "Aplicación Web Fullstack",
+        title: "Plataforma web ecommerce de comida",
+        description: "Desarrollo de una pagina web personalizada para un restaurante, donde el cliente puede realizar pedidos, ver el menu, gestionar su cuenta y realizar pagos en linea.",
+        stack: [{ name: "Javascript" }, { name: "Tailwind" }, { name: "React" }, { name: "Laravel" }, { name: "Mysql" }],
+        image: "/assets/work/work4.png",
+        live: "https://ecommerce-juan-web.netlify.app/",
+        github: "https://github.com/juanjosebl2/ecommerce-frontend-react"
+    },
+    {
+        num: "05",
+        category: "Aplicación Web Frontend",
+        title: "Mi primer portfólio",
+        description: "Desarrolo de mi primer portafolio personal, donde muestro mis proyectos, habilidades y experiencia.",
+        stack: [{ name: "Javascript" }, { name: "Css" }, { name: "React" }],
+        image: "/assets/work/work5.png",
+        live: "https://ecommerce-juan-web.netlify.app/",
+        github: "https://github.com/juanjosebl2/portfolio-react"
+    },
+    {
+        num: "06",
+        category: "Aplicación Web Frontend",
+        title: "Plataforma web de peliculas",
+        description: "Desarrollo de una pagina web personalizada para ver peliculas, con un diseño moderno y atractivo, usando una API para obterner las peliculas.",
+        stack: [{ name: "Javascript" }, { name: "Css" }, { name: "React" }],
+        image: "/assets/work/work6.png",
+        live: "https://movie-juan-web.netlify.app/",
+        github: "https://github.com/juanjosebl2/movie-react"
+    },
+    {
+        num: "07",
+        category: "Aplicación Web Backend",
+        title: "Crud de usuarios con Nodejs, spring boot",
+        description: "Desarrollo de un crud de usuarios con Nodejs y spring boot, donde se puede crear, leer, actualizar y eliminar usuarios.",
+        stack: [{ name: "Java" }, { name: "Nodejs" }, { name: "Spring boot" }, { name: "MongoDB" }],
+        image: "/assets/work/work7.png",
         live: "",
-        github: ""
+        github: "https://github.com/juanjosebl2/springBoot-MongoDB"
+    },
+    {
+        num: "08",
+        category: "Aplicación Web Backend",
+        title: "Crud de usuarios con Laravel, incluye interfaz dashboard",
+        description: "Desarrollo de un crud de usuarios con Laravel, donde se puede crear, leer, actualizar y eliminar usuarios, incluye un dashboard para gestionar los usuarios.",
+        stack: [{ name: "Laravel" }, { name: "Mysql" }, { name: "PHP" }],
+        image: "/assets/work/work8.png",
+        live: "",
+        github: "https://github.com/juanjosebl2/laravel-fichaTecnica"
+    },
+    {
+        num: "09",
+        category: "Juego",
+        title: "Juego de memoria",
+        description: "Desarrollo de un juego de memoria, donde el jugador debe recordar la posición de las cartas y hacer parejas.",
+        stack: [{ name: "C#" }, { name: "Mysql" }],
+        image: "/assets/work/work9.png",
+        live: "",
+        github: "https://github.com/juanjosebl2/Juego_Memoria"
     },
 ]
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -26,7 +26,10 @@ export default function Page() {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1, transition: { delay:2.4, duration: 0.4, ease: "easeIn" } }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
@@ -36,11 +39,11 @@ export default function Page() {
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
               </div>
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                {project.category} proyecto
+              <h2 className="text-[32px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category}
               </h2>
               <p className="text-white/60">{project.description}</p>
-              <ul className="flex gap-4">
+              <ul className="flex flex-col gap-4 xl:flex-row">
                 {project.stack.map((item, index) => (
                   <li key={index} className="text-accent text-xl">
                     {item.name}
@@ -50,30 +53,34 @@ export default function Page() {
               </ul>
               <div className="border border-white/20"></div>
               <div className="flex items-center gap-4">
-                <Link href={project.live}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Pagina del proyecto</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
-                <Link href={project.github}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Github</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                {project.live && project.live !== "" && (
+                  <Link href={project.live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Pagina del proyecto</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                )}
+                {project.github && project.github !== "" && (
+                  <Link href={project.github}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Github</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
@@ -81,27 +88,32 @@ export default function Page() {
             <Swiper
               spaceBetween={0}
               slidesPerView={1}
-              className="xl:h-[520px] mb-12"
+              className="xl:h-[520px] mb-12 hover:img-shadow2 transition-all duration-500"
               onSlideChange={handleSlideChange}
             >
               {dataWorks.map((item, index) => (
                 <SwiperSlide key={index} className="w-full">
                   <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10">
-                      {/* {item.title} */}
+                    <div className="absolute left-4 top-4 bottom-0 w-8 h-8 bg-black/10 z-10">
+                      <Image
+                        src="/assets/work/slide.png"
+                        fill
+                        className="rounded-xl"
+                        alt={item.title}
+                      />
                     </div>
                     <div className="relative w-full h-full">
                       <Image
                         src={item.image}
                         fill
-                        className="object-cover"
+                        className="cover"
                         alt={item.title}
                       />
                     </div>
                   </div>
                 </SwiperSlide>
               ))}
-              <WorkSliderButton  
+              <WorkSliderButton
                 constainerStyles="flex gap-2 absolute right-0 bottom-[calc)=(50%-22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
                 iconsStyles=""
